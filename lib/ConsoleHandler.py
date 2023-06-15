5ae10df0b880bcc87b8027ae9432100e8e551ffb
k3ystr0k3r <70909693+K3ysTr0K3R@users.noreply.github.com>
2023-06-15 10:11:37 -0600

    Update ConsoleHandler.py

--- lib/ConsoleHandler.py
+++ lib/ConsoleHandler.py
@@ -1,4 +1,7 @@
 from termcolor import colored
+from datetime import datetime
+
+time_date = datetime.now().strftime("%H:%M:%S")
 
 class ConsoleHandler:
     def __init__(self):
@@ -12,3 +15,6 @@ class ConsoleHandler:
 
     def info(self, ip_address, server_name, response_code):
         print(colored('[*] ', 'yellow') + colored(ip_address, 'cyan') + f" [{server_name}] [{response_code}]")
+
+    def more_info(self, subdomain_url_response_url):
+        print(colored('[' + time_date + '] ', 'cyan') + colored('[INFO]', 'green') + colored(' ['+ subdomain_url_response_url  + ']'))
